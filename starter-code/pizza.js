b37ca99da7745c31c182a74c8169abc61b849ac5
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-10-27 08:55:09 -0400

    Added on/off functionality to 3 buttons (pep,mushroom,peppers).

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,67 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function (){
+
+
+var button = "on";
+//Event Handler - Pepperoni Button:
+$('.btn-pepperonni').click( function (){
+    if (button === "on") {
+      $('.pep').hide();
+      button = "off";
+    } else {
+      $('.pep').show();
+      button = "on";
+    }
+}); //Button End
+
+
+
+//Event Handler - Mushroom Button:
+$('.btn-mushrooms').click(function(){
+    if (button === "on") {
+      $('.mushroom').hide();
+      button = "off";
+    } else {
+      $('.mushroom').show();
+      button = "on";
+    }
+});//Button End
+
+
+
+//Event Handler - Green Peppers Button:
+$('.btn-green-peppers').click(function(){
+    if (button === "on") {
+      $('.green-pepper').hide();
+      button = "off";
+    } else {
+      $('.green-pepper').show();
+      button = "on";
+  }
+});//Button End
+
+
+
+//Event Handler - Sauce Button:
+$('.btn-sauce').click(function(){
+
+
+
+});//Button End
+
+
+
+//Event Handler - Crust Button:
+$('.btn-crust').click(function(){
+
+
+
+});//Button End
+
+
+
+
+
+
+
+
+});//Doctument Ready
